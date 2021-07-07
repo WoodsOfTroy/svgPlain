498b8c64b7b0254a083a783d4f4f44cd6c057ffc
Ruslan <58141049+WoodsOfTroy@users.noreply.github.com>
2021-07-07 12:58:15 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
-let cost = document.querySelectorAll(".place-cost");
-let g = document.querySelectorAll("g");
+let g = document.querySelectorAll(".g-wrap");
+window.onload = function(){
+    g.forEach(item => {
+        if(item.classList.contains("place-disabled"))
+        {
+            item.classList.add("reserved");
+        }
+    });
+}
 function changerect(e)
 {
-    if(e.target && e.target.parentNode.classList.contains("place"))
-    {
-        console.dir(e);
-        if(confirm(`Вы уверенны что хотите забронировать место?`))
+    console.dir(e.target.parentNode.classList);
+    
+        if(e.target.parentNode.classList.contains("place"))
         {
-            e.target.parentNode.classList.remove("place");
-            e.target.parentNode.classList.add("place-disabled");
-            cost.forEach((item, i) => {
-                if(item.parentNode.classList.contains("place-disabled"))
-                {
-                    item.textContent = "0";
-                }
-            });
+            if(confirm("Хотите забронировать место?"))
+            {
+                g.forEach(item => {
+                    if(item.classList.contains("place-disabled") && !item.classList.contains("reserved"))
+                    {
+                        item.classList.remove("place-disabled");
+                        item.classList.add("place");
+                    } else {
+                        e.target.parentNode.classList.remove("place");
+                        e.target.parentNode.classList.add("place-disabled");
+                    }
+                });
+            }
+            
+        } else if(e.target.parentNode.classList.contains("place-disabled") && !e.target.parentNode.classList.contains("reserved"))
+        {
+            if(confirm("Хотите снять бронь?"))
+            {
+                e.target.parentNode.classList.remove("place-disabled");
+                e.target.parentNode.classList.add("place");
+            }  
+        } else if(e.target.parentNode.classList.contains("reserved"))
+        {
+            alert("Место забронировано");
         }
-    } else {
-        alert("Место уже забронировано");
-    }
 }
